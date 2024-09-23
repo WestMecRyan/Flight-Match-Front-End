24810b207b5802ed11d57ff77ae78ce094b536ac
WestMecRyan <ryan.morales@west-mec.org>
2024-09-22 22:35:07 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Flight-Match-Front-End
 The client-side for Flight Match
+
+# Install Modules
+`bun i`
+`bun run dev`
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>Flight Match</div>
+    </>
+  )
+}
+
+export default App

2e8bbdd0dab35c0ac36a8e34f9fc8a25d73497a1
Ryan Morales <ryan.morales@west-mec.org>
2024-09-23 06:54:10 +0000

    updated env

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL 
+// ||  'https://orange-space-palm-tree-xj99v45pg45cpj6r-3000.app.github.dev';
 
 export const api = axios.create({
-    baseURL: API_URL
+    baseURL: API_URL,
+    withCredentials: true
 });
 
 export const fetchWelcomeMessage = async () => {
